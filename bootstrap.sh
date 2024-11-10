643b9bb14b5a8d98b5f5aaf13b7894bba80f6dff
Chris Crain <22624146+chrislcrain@users.noreply.github.com>
2024-11-09 23:58:28 -0500

    additional fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,7 @@ git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME fetch --all
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME reset --hard
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --force --recursive --init --remote
 
+sudo apt update -y
 sudo apt install zsh -y
 ZSH=$HOME/.config/ohmyzsh/.oh-my-zsh ZSH_CUSTOM=$HOME/.config/ohmyzsh/.zsh_custom sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cp ~/.zshrc.pre-oh-my-zsh ~/.zshrc
