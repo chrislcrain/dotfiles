5752fcf691b9fadeea097a8803550be1789871a8
Chris Crain <22624146+chrislcrain@users.noreply.github.com>
2024-11-09 23:27:09 -0500

    additional fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
 cd ~
 
 # Uncomment this if setting up for WSL - 1Password integration
@@ -15,8 +14,7 @@ git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME fetch --all
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME reset --hard
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --force --recursive --init --remote
 
-sudo apt update
-
+sudo apt install zsh -y
 ZSH=$HOME/.config/ohmyzsh/.oh-my-zsh ZSH_CUSTOM=$HOME/.config/ohmyzsh/.zsh_custom sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cp ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
