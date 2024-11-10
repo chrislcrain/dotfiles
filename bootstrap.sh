71d09fbf7a98a6bbec3004db8ac4082e532a900c
Chris Crain <22624146+chrislcrain@users.noreply.github.com>
2024-11-09 23:14:19 -0500

    additional fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-sh $HOME/bin/dotfiles.sh
-sh $HOME/bin/appconfig.sh
+#!/bin/bash
+cd ~
+
+# Uncomment this if setting up for WSL - 1Password integration
+# git config --global core.sshCommand ssh.exe
+
+git config --global user.email "22624146+chrislcrain@users.noreply.github.com"
+git config --global user.name "Your Name"
+
+git clone --bare git@github.com:chrislcrain/dotfiles.git $HOME/.dotfiles
+
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME fetch --all
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME reset --hard
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --force --recursive --init --remote
+
+sudo apt update
+
+ZSH=$HOME/.config/ohmyzsh/.oh-my-zsh ZSH_CUSTOM=$HOME/.config/ohmyzsh/.zsh_custom sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+cp ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+
+# Set zsh as default shell
+sudo chsh -s $(which zsh)
+
+exec zsh
