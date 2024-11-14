633a4fa3ee60b1a44c34e90bb1831d8018c9220a
Chris Crain <22624146+chrislcrain@users.noreply.github.com>
2024-11-14 11:57:10 -0500

    adding ripgrep and prioritizing powershell_es during debugging

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,7 +26,7 @@ sudo make install
 sudo apt-get install npm -y
 sudo apt-get install luarocks -y
 sudo apt-get install python3-venv -y
-sudo apt-get install rip-grep -y
+sudo apt-get install ripgrep -y
 
 # Install PowerShell
 sudo apt-get install -y wget apt-transport-https software-properties-common
