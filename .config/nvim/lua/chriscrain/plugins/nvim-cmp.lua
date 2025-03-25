e1c25995b17e2d03d1476537fbd7ea78bf70bb94
Chris Crain <22624146+chrislcrain@users.noreply.github.com>
2025-03-25 15:26:13 -0400

    copilot and friends

--- .config/nvim/lua/chriscrain/plugins/nvim-cmp.lua
+++ .config/nvim/lua/chriscrain/plugins/nvim-cmp.lua
@@ -19,7 +19,24 @@ return {
     local cmp = require("cmp")
     local luasnip = require("luasnip")
     local lspkind = require("lspkind")
-
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+      end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+    end
+    cmp.setup({
+      mapping = {
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
+      },
+    })
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -32,6 +49,24 @@ return {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
+      },
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          require("copilot_cmp.comparators").prioritize,
+
+          -- Below is the default comparitor list and order for nvim-cmp
+          cmp.config.compare.offset,
+          -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.locality,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
       },
       mapping = cmp.mapping.preset.insert({
         ["<Tab>"] = cmp.mapping.select_next_item(), -- previous suggestion
@@ -53,8 +88,11 @@ return {
 
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
+        expandable_indicator = true,
         format = lspkind.cmp_format({
-          maxwidth = 50,
+          mode = "symbol",
+          max_width = 50,
+          symbol_map = { Copilot = "" },
           ellipsis_char = "...",
         }),
       },
