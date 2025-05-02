9a48155ced2945a7e33be6c624cfc4369c58f934
chriscrain <22624146+chrislcrain@users.noreply.github.com>
2025-05-02 15:36:35 -0400

    fix .profile

--- dot_config/nvim/lua/plugins/lsp/mason.lua
+++ dot_config/nvim/lua/plugins/lsp/mason.lua
@@ -1,6 +1,7 @@
 return {
   "williamboman/mason.nvim",
   dependencies = {
+    "rcarriga/nvim-notify",
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
