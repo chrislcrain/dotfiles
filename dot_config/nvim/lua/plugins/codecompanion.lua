0983e0fc051629e94a428d1ef9f5132bc9f66f57
chriscrain <22624146+chrislcrain@users.noreply.github.com>
2026-02-04 23:56:27 -0500

    fix codecompanion

--- dot_config/nvim/lua/plugins/codecompanion.lua
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -1,5 +1,6 @@
 return {
   enabled = true,
+  version = "18.5.1",
   "olimorris/codecompanion.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -33,13 +34,33 @@ return {
           },
         },
       },
-
       display = {
         chat = {
+          variables = {
+            ["buffer"] = {
+              opts = {
+                -- Always sync the buffer by sharing its "diff"
+                -- Or choose "all" to share the entire buffer
+                default_params = "diff",
+              },
+            },
+          },
+          icons = {
+            chat_context = "📎️",
+            chat_fold = " ",
+          },
+          auto_scroll = true,
+          show_context = true,
+          fold_context = false,
+          show_reasoning = true,
+          fold_reasoning = true,
+          show_settings = true,
           start_in_insert_mode = false,
           intro_message = "Beep boop! 🤖 Happy coding!",
           show_header_separator = false,
-          separator = "=",
+          show_tools_processing = true,
+          show_token_count = true,
+          separator = "-",
           keymaps = {
             choose_adapter = "<leader>ca",
           },
@@ -49,35 +70,16 @@ return {
           -- close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
           layout = "vertical", -- vertical|horizontal split for default provider
           opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
-          provider = "mini_diff", -- default|mini_diff
+          provider = "default", -- default|mini_diff
         },
       },
-      strategies = {
+      interactions = {
         chat = {
-          default_context = { "file" },
-          slash_commands = {
-            ["file"] = {
-              -- Location to the slash command in CodeCompanion
-              callback = "strategies.chat.slash_commands.file",
-              description = "Select files using Telescope",
-              opts = {
-                provider = "telescope", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
-                contains_code = true,
-                multi_select = true, -- Enable multi-selection
-              },
-            },
-          },
-          ["buffer"] = {
-            description = "Select a buffer using Telescope",
-            opts = {
-              provider = "telescope", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
-              contains_code = true,
-            },
-          },
+          default_context = { "buffer" },
+          adapter = "copilot",
         },
-        adapter = "copilot",
+        inline = { adapter = "copilot" },
       },
-      inline = { adapter = "copilot" },
     })
   end,
 }
